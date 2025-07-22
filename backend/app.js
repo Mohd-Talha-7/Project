694fa241c0335d023ff67bc6e3ba857aee55f6ae
Mohd Talha <talhalko1@gmail.com>
2025-07-22 19:27:41 +0530

    Updating cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://project-lovat-nine-86.vercel.app",
+    ],
     credentials: true,
   })
 );
